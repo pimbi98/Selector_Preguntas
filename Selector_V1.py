a370ced875c474b0857b7659745545a7caf573af
pimbi98 <pimbi998@gmail.com>
2023-08-30 12:39:55 -0300

    Preguntas Realizadas
    Se añade control de preguntas realizadas.

--- Selector_V1.py
+++ Selector_V1.py
@@ -1,44 +1,77 @@
 #dado un rango de numeros, se genera un control para las preguntas dadas,
 import random
-numeros = list()
-def Manual(numeros):
+import time
+preguntas = list()
+preguntasRealizadas = list()
+def addPreguntasRealizadas(p): #Añade a lista de preguntas ya realizadas.
+    preguntasRealizadas.append(p)
+
+def printPreguntasRealizadas():
+    print("Se hicieron " + str(len(preguntasRealizadas)) + " Preguntas")
+    print("Preguntas realizadas en orden realizado: " + str(preguntasRealizadas))
+
+
+def printPreguntasRestantes(l):
+    print("Quedan " + str(len(l)) + " Preguntas")
+    print("Lista actual: " + str(l))
+
+def Manual(preguntas):
     while 1==1:
-        NQuitar = int(input("Ingrese numero a quitar de la lista o ingrese 0 para salir: "))
-        if NQuitar > 0:
-            numeros.remove(NQuitar)        
-        elif NQuitar == 0:
+        printPreguntasRestantes(preguntas)
+        nro = int(input("Ingrese numero a quitar de la lista o ingrese 0 para salir: "))
+        if nro > 0:
+            addPreguntasRealizadas(nro)
+            preguntas.remove(nro)        
+        elif nro == 0:
             return
         else: 
             print("Valor incorrecto, ingrese nuevamente.")
 
 
-def Aleatorio(numeros):
-    r = random.randint(1, len(numeros))
-    print("Numero a quitar aleatoriamente es: " + str(numeros[r]))
-    numeros.pop(r)     
-
+def Aleatorio(preguntas):
+    r = random.randint(1, len(preguntas))
+    nro = preguntas[r]
+    print("Numero a quitar aleatoriamente es: " + str(nro))
+    addPreguntasRealizadas(nro)
+    preguntas.pop(r)
+    time.sleep(1)
+##---------------------- 0 ------------------------------##
+NErrores = 0
 NDesde = int(input("Ingrese numero desde: "))
 NHasta = int(input("Ingrese numero hasta: "))
 
 for i in range(NDesde, NHasta):
-    numeros.append(i)
+    preguntas.append(i)
 
 
 while 1==1:
-    print("Lista actual: " + str(numeros))
+    printPreguntasRestantes(preguntas)
     print("Acciones que se pueden realizar: ")
     print("M: Quitar numeros Manualmente. ")
     print("A: Quitar numeros Aleatoriamente. ")
     print("P: Imprimir lista actual. ")
+    print("R: Mostrar preguntas realizadas. ")
     print("X: Salir del programa. ")
     Accion = input("Ingrese Acción: ")
-    if Accion == "M": #Saco numeros de lista manualmente.
-        Manual(numeros)
+    if Accion == "M": #Saco preguntas de lista manualmente.
+        Manual(preguntas)
     elif Accion == 'A': #Aleatorios
-        Aleatorio(numeros)
+        Aleatorio(preguntas)
     elif Accion == 'P': #Aleatorios
-        print(numeros)
+        printPreguntasRestantes(preguntas)
     elif Accion == 'X':
         break
+    elif Accion == 'R':
+        printPreguntasRealizadas()
+    else:
+        NErrores = NErrores + 1
+        print("Valor incorrecto.")
+        if NErrores >= 3:
+            print("Se equivocó demasiadas veces. Finalizando programa... ")
+            time.sleep(3)
+            break
 
 print("Programa finalizado.")
+
+#Desde: 65 hasta:128
+#69,74,77,78,83,100,101,113,119,125,67,114
